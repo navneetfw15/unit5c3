292bcfb8cb839b7d18b5cc93b779bf79891645b9
navneetfw15 <navneet638864@gmail.com>
2022-04-25 16:51:32 +0530

    text file

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,30 @@ import { Login } from "./components/Login";
 import { EmployeeList } from "./components/EmployeeList";
 import { EmployeeDetails } from "./components/EmployeeDetails";
 import { Admin } from "./components/Admin";
-
+// import { ProtectedRoute } from "./components/PrivateRoute";
 import { Navbar } from "./components/Navbar";
-
-
 import { Logout } from "./components/Logout";
-
-import {Routes,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element ={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/employees" element={<EmployeeList />}></Route>
+        <Route
+          exact
+          path="/employees/:id"
+          element={<EmployeeDetails></EmployeeDetails>}
+        ></Route>
 
-        <Route path="/employees" element={<EmployeeList/>}/>
-
-        <Route path="/employees/:id" element={<EmployeeDetails/>}/>
-        
-        <Route path="/admin" element={<Admin/>}/>
-        <Route path="/logout" element={<Logout/>}/>
+        <Route exact path="/admin" element={<Admin />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/logout" element={<Logout />}></Route>
       </Routes>
     </div>
   );
 }
 
 export default App;
-
--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,4 +1,4 @@
 export const Logout = () => {
-    // log user out. it's just an inmemory value in context api
-    return <div></div>;
-  };
+  // log user out. it's just an inmemory value in context api
+  return <div></div>;
+};
